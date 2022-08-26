78e0ffd51ec537b738dc0b98efe96be3d7d26a3b
souley224 <diallosouleymanekolon@gmail.com>
2022-08-26 14:08:19 +0200

    fix services get pair with axios

--- src/services/pairsService.js
+++ src/services/pairsService.js
@@ -3,20 +3,70 @@ import { ref } from "vue";
 import router from '../router';
 
 export default function usePairs() {
+  const errors = ref('');
   const pair = ref('');
   const pairs = ref([]);
 
+  // récuperer la liste de toutes les paires disponible
   const getPairs = async () => {
     let response = await axios.get('http://127.0.0.1:8000/api/pairs');
 
     pairs.value = response.data.data;
+  };
 
-    console.log(pairs.value);
+  // methode pour recuperer une paire à partir d'un id
+  const getPair = async (id) => {
+    let response = await axios.get('http://127.0.0.1:8000/api/pairs/' +id);
+    pair.value = response.data.data;
+  };
+
+  // methode pour créer une pair
+  const createPair = async (data) => {
+    errors.value = '';
+
+    try {
+      let response = await axios.post('http://127.0.0.1:8000/api/pairs', data);
+
+      await router.push({ name: 'pairs.index'});
+    } catch (error) {
+      const createRegisterErrors = error.response.data.errors;
+      errors.value = createRegisterErrors;
+    }
+
+  };
+
+  // methode pour modifier une paire
+  const updatePair = async (id) => {
+    errors.value = '';
+
+    try {
+      let response = await axios.put('http://127.0.0.1:8000/api/pairs/' + id, pair.value);
+
+      console.log(response.data);
+      await router.push({ name: 'pairs.index'});
+    } catch (error) {
+      const createRegisterErrors = error.response.data.errors;
+      errors.value = createRegisterErrors;
+    }
+
+  };
+
+  // methode pour supprimer une paire
+  const destroyPair = async (id) => {
+    if (!window.confirm('Voulez - vous Supprimer cette paire ?')) return;
+
+    await axios.delete('http://127.0.0.1:8000/api/pairs/' + id);
+    await getPairs();
   };
 
   return {
     pair,
     pairs,
-    getPairs
+    errors,
+    getPairs,
+    getPair,
+    createPair,
+    updatePair,
+    destroyPair,
   }
 }
