749a9fcafec3947179ee52be5d99a39724e0f396
souley224 <diallosouleymanekolon@gmail.com>
2022-08-28 23:33:34 +0200

    fix login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -20,8 +20,8 @@
         </div>
         <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-          Not registered? <router-link to="/register" class="text-blue-700 hover:underline dark:text-blue-500">Create
-            account</router-link>
+          Not registered? <router-link to="/register" class="text-blue-700 hover:underline dark:text-blue-500">CONNEXION
+          </router-link>
         </div>
       </form>
     </div>
